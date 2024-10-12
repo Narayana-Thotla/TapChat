488711b8d182e255cc708322d7b28b5e3b22624b
Thotla Lakshmi Narayana <lakshminarayanathotla@gmail.com>
2024-10-12 16:37:27 +0530

    cors For socket

--- backend/socket/Socket.js
+++ backend/socket/Socket.js
@@ -4,11 +4,20 @@ import { createServer } from "node:http";
 
 const app = express();
 const server = createServer(app);
+
+
+const allowedOrigins = ['http://localhost:5173', 'https://tapchat-34hc.onrender.com'];
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Allow frontend server
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ["GET", "POST"],
-    credentials: true 
+    credentials: true,  // Allows cookies and other credentials
   },
 });
 
